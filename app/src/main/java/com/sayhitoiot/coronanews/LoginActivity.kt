6e73d9739927e335364808b2a64f1324ddcf32a2
evd-evanss <revandro77@yahoo.com.br>
2020-05-15 00:12:41 -0300

    business logic for login screen created

--- app/src/main/java/com/sayhitoiot/coronanews/LoginActivity.kt
+++ app/src/main/java/com/sayhitoiot/coronanews/LoginActivity.kt
@@ -1,13 +1,75 @@
 package com.sayhitoiot.coronanews
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.EditText
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.button.MaterialButton
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.activity_login.*
+
 
 class LoginActivity : AppCompatActivity() {
+
+    companion object {
+        const val TAG = "login-activity"
+    }
+
+    private var edtEmail: EditText? = null
+    private var edtPassword: EditText? = null
+    private var mAuth: FirebaseAuth? = null
+    private var btnLogin: MaterialButton? = null
+    private var btnSignUp: MaterialButton? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        mAuth = FirebaseAuth.getInstance()
+        initializeViews()
+    }
+
+    private fun initializeViews() {
+        edtEmail = activityLogin_editText_email
+        edtPassword = activityLogin_editText_password
+        btnLogin = loginActivity_materialButton_login
+        btnSignUp = loginActivity_materialButton_signUp
+        initializeListeners()
+    }
+
+    private fun initializeListeners() {
+        btnLogin?.setOnClickListener {
+            signWithUserAndPassword(
+                edtEmail?.text.toString(),
+                edtPassword?.text.toString()
+            )
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        val currentUser = mAuth?.currentUser
+        //updateUI(currentUser)
+    }
+
+    private fun signWithUserAndPassword(email: String, password: String) {
+        mAuth?.signInWithEmailAndPassword(email, password)
+            ?.addOnCompleteListener(
+                this
+            ) { task ->
+                if (task.isSuccessful) {
+                    Log.d(TAG, "signInWithEmail:success")
+                    val user = mAuth?.currentUser
+                    //updateUI(user)
+                } else {
+                    Log.w(TAG, "signInWithEmail:failure", task.exception)
+                    Toast.makeText(
+                        this@LoginActivity, "Authentication failed.",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                    //updateUI(null)
+                }
+            }
     }
 
 }
